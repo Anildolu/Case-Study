63b7345d170977cbc097c978884041b0aa07f3cd
adolu <adolu@ezesoft.com>
2018-12-24 21:11:11 +0530

    updated the scripts

--- UI_Protractor/pageObject/gitlogin.js
+++ UI_Protractor/pageObject/gitlogin.js
@@ -7,5 +7,11 @@ module.exports = {
         signin: element(by.css('[href="/login"]')),
         username: element(by.id('login_field')),
         password: element(by.id('password')),
+        signinbutton: element(by.css('.btn.btn-primary.btn-block')),
+        forgetpassword:element(by.css('[href="/password_reset"]')),
+        forgetpasswordtextbox:element(by.id('email_field')),
+        password_reset_emailsend_button:element(by.css('.btn.btn-primary.btn-block')),
+        return_signin_button:element(by.css('[href="/login"]')),
+        Create_account_link: element(by.css(".create-account-callout.mt-3"))
     },
 }
--- UI_Protractor/specs/gitlogin_spec.js
+++ UI_Protractor/specs/gitlogin_spec.js
@@ -4,22 +4,62 @@ var gitloginPage = require('../pageObject/gitlogin.js');
 
 describe("Gitlogin Page testing", function () {
     beforeAll(function (done) {
+        console.log(`-------------- ${global.testCaseName} --------------`);
         browser.ignoreSynchronization = true;
         browser.get('https://github.com');
         done();
     });
 
-    it('Testing the Git login', function () {
-        console.log(`-------------- ${global.testCaseName} --------------`);
+    it('Testing the sign in button', function () {
+        console.log(`${global.testStepName}`);
         gitloginPage.elements.signin.click();
         expect(browser.getCurrentUrl()).toBe('https://github.com/login');
-        browser.sleep(3000);
     });
 
+    it('Testing the login without username and password', function () {
+        console.log(`${global.testStepName}`);
+        gitloginPage.elements.signinbutton.click();
+        expect(browser.getCurrentUrl()).toBe('https://github.com/session');
+    });
+    it('Testing login with username and  without password', function () {
+        console.log(`${global.testStepName}`);
+        gitloginPage.elements.username.sendkeys("doluanil@gmail.com")
+        gitloginPage.elements.signinbutton.click();
+        expect(browser.getCurrentUrl()).toBe('https://github.com/session');
+    });
+
+    it('Testing login without username and  with password', function () {
+        console.log(`${global.testStepName}`);
+        gitloginPage.elements.password.sendkeys("Anil@1218")
+        gitloginPage.elements.signinbutton.click();
+        expect(browser.getCurrentUrl()).toBe('https://github.com/session');
+    });
+
+    it('Testing the forgot password', function () {
+        console.log(`${global.testStepName}`);
+        gitloginPage.elements.username.sendkeys("doluanil@gmail.com")
+        gitloginPage.elements.forgetpassword.click();
+        expect(browser.getCurrentUrl()).toBe('https://github.com/password_reset');
+        gitloginPage.elements.forgetpasswordtextbox.sendkeys('anil@gmail.com');
+        gitloginPage.elements.password_reset_emailsend_button.click();
+        gitloginPage.elements.return_signin_button.click();
+    });
+
+    it('Testing the creation of new account', function () {
+        console.log(`${global.testStepName}`);
+        gitloginPage.elements.Create_account_link.click();
+        expect(browser.getCurrentUrl()).toBe('https://github.com/join?source=login');
+        gitloginPage.elements.signinbutton.click();
+        
+    });
+    
+
     it('Testing the Git login', function () {
-        console.log(`-------------- ${global.testCaseName} --------------`);
-       gitloginPage.elements.signin.click();
-       browser.sleep(3000);
+        console.log(`${global.testStepName}`);
+        gitloginPage.elements.username.sendkeys("doluanil@gmail.com")
+        gitloginPage.elements.password.sendkeys("Anil@1218")
+        gitloginPage.elements.signinbutton.click();
+        expect(browser.getCurrentUrl()).toBe('https://github.com');
     });
 
 });
