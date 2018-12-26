f0309084c7c7560de85c2b752e99071f4af8e49c
adolu <adolu@ezesoft.com>
2018-12-26 16:04:13 +0530

    Updated the script

---
+++ API_Testing/spec/gitGET_spec.js
@@ -0,0 +1,46 @@
+// Require the dev-dependencies
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+ const config = require('../config');
+// const helperInstance = require('../auth');
+const expect = chai.expect;
+
+chai.should();
+chai.use(chaiHttp);
+
+// Get the URL for counterparty API
+let restURL = function () {
+  return config.restBaseUrl + '/';
+};
+
+  describe('GET - Negative scenarios tests', () => {
+   
+    it(`TC-01 : test to create the new`, (done) => {
+      chai.request(restURL())
+        .get('repos/Anildolu/Case-Study/commits')
+        .set('Authorization', config.basicAuth)
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+
+    // it(`TC-01 : test to create the new`, (done) => {
+    //   let postpayload1={ 
+    //     base: "master",
+    //     head: "Anildolu-patch-5",
+    //     commit_message: "Shipped cool_feature!"
+    //       };
+    //   chai.request(restURL())
+    //     .post('repos/Anildolu/Case-Study/merges')
+    //     .send(postpayload1)
+    //     .set('Authorization', config.basicAuth)
+    //     .end((err, res) => {
+    //       console.log(JSON.stringify(res));
+    //       res.should.have.status(404);
+    //       res.body.message
+    //       expect(res.body.message).to.contains("Head does not exist");
+    //       done();
+    //     });
+    // });
+});
---
+++ UI_Protractor/scripts/gitloginscript.js
@@ -0,0 +1,56 @@
+'use strict';
+var gitloginPage = require('../pageObject/gitlogin.js')
+
+
+module.exports = {
+
+    gitLoginUrl: function () {
+        browser.get('https://github.com');
+    },
+
+    gitSigninButton: function () {
+        gitloginPage.elements.signin.click();
+        expect(browser.getCurrentUrl()).toBe('https://github.com/login');
+    },
+
+    gitlogin_withoutusername_password: function () {
+        gitloginPage.elements.signinbutton.click();
+        expect(browser.getCurrentUrl()).toBe('https://github.com/session');
+    },
+
+    gitlogin_withusername_withoutpassword: function () {
+        gitloginPage.elements.username.sendKeys("doluanil@gmail.com")
+        gitloginPage.elements.signinbutton.click();
+        expect(browser.getCurrentUrl()).toBe('https://github.com/session');
+        gitloginPage.elements.username.clear();
+    },
+
+    gitlogin_withoutusername_withpassword: function () {
+        gitloginPage.elements.password.sendKeys("Anil@1218")
+        gitloginPage.elements.signinbutton.click();
+        expect(browser.getCurrentUrl()).toBe('https://github.com/session');
+        gitloginPage.elements.password.clear();
+    },
+
+    git_forgot_password: function () {
+        gitloginPage.elements.username.sendKeys("doluanil@gmail.com")
+        gitloginPage.elements.forgetpassword.click();
+        expect(browser.getCurrentUrl()).toBe('https://github.com/password_reset');
+        gitloginPage.elements.forgetpasswordtextbox.sendKeys('anil@gmail.com');
+        gitloginPage.elements.password_reset_emailsend_button.click();
+        gitloginPage.elements.return_signin_button.click();
+    },
+
+    git_creation_newaccount: function () {
+        gitloginPage.elements.Create_account_link.click();
+        expect(browser.getCurrentUrl()).toBe('https://github.com/join?source=login');
+        gitloginPage.elements.signin_button_create_account.click();
+    },
+
+    git_login: function () {
+        gitloginPage.elements.username.sendKeys("doluanil@gmail.com")
+        gitloginPage.elements.password.sendKeys("Anil@1218")
+        gitloginPage.elements.signinbutton.click();
+        expect(browser.getCurrentUrl()).toBe('https://github.com/');
+    }
+}
