28c3893bcc05d346bc13f3a3a1d404259863f2c8
adolu <adolu@ezesoft.com>
2019-01-02 19:03:43 +0530

    updated the scripts

--- API_Testing/spec/gitGET_spec.js
+++ API_Testing/spec/gitGET_spec.js
@@ -22,29 +22,77 @@ let restURL = function () {
         .set('Authorization', config.basicAuth)
         .end((err, res) => {
           res.should.have.status(200);
-          res.body[0].should.be.eql(gitExpect);
+          expect(res.body[0].commit.author.name).to.be.equal("anil");
+          expect(res.body[0].commit.author.email).to.be.equal("doluanil@gmail.com");
+          expect(res.body[0].commit.committer.name).to.be.equal("anil");
+          expect(res.body[0].commit.committer.email).to.be.equal("doluanil@gmail.com");
+          expect(res.body[0].commit.message).to.be.equal("updated the credintioals");
           done();
         });
     });
 
-    it(`TC-01 : test the git contribiters`, (done) => {
+    it(`TC-02 : test the git contribiters`, (done) => {
       chai.request(restURL())
         .get('repos/Anildolu/Case-Study/contributors')
         .set('Authorization', config.basicAuth)
         .end((err, res) => {
-          res.should.have.status(204);
+          res.should.have.status(200);
+          expect(res.body[0].login).to.be.equal("Anildolu");
+          expect(res.body[0].id).to.be.equal(25811285);
+          expect(res.body[0].node_id).to.be.equal("MDQ6VXNlcjI1ODExMjg1");
+          expect(res.body[0].avatar_url).to.be.equal("https://avatars0.githubusercontent.com/u/25811285?v=4");
+          expect(res.body[0].type).to.be.equal("User");
+          expect(res.body[0].site_admin).to.be.equal(false);
+          expect(res.body[0].contributions).to.be.equal(14);
           done();
         });
     });
 
-    it(`TC-01 : test the git repo language`, (done) => {
+    it(`TC-03 : test the git repo language`, (done) => {
       chai.request(restURL())
         .get('repos/Anildolu/Case-Study/languages')
         .set('Authorization', config.basicAuth)
         .end((err, res) => {
           res.should.have.status(200);
-          expect(res.body.JavaScript).to.be.equal(13123);
+          expect(res.body.JavaScript).to.be.equal(14802);
           done();
         });
     })
+    
+    it(`TC-04 : test the git repos `, (done) => {
+      chai.request(restURL())
+        .get('users/Anildolu/repos')
+        .set('Authorization', config.basicAuth)
+        .end((err, res) => {
+          res.should.have.status(200);
+          expect(res.body[0].id).to.be.equal(162714285);
+          expect(res.body[0].node_id).to.be.equal("MDEwOlJlcG9zaXRvcnkxNjI3MTQyODU=");
+          expect(res.body[0].name).to.be.equal("Case-Study");
+          expect(res.body[0].full_name).to.be.equal("Anildolu/Case-Study");
+          expect(res.body[0].private).to.be.equal(false);
+          expect(res.body[1].id).to.be.equal(162962409);
+          expect(res.body[1].node_id).to.be.equal("MDEwOlJlcG9zaXRvcnkxNjI5NjI0MDk=");
+          expect(res.body[1].name).to.be.equal("Selenium_Test");
+          expect(res.body[1].full_name).to.be.equal("Anildolu/Selenium_Test");
+          expect(res.body[1].private).to.be.equal(false);
+          done();
+        });
+    })
+    it(`TC-05 : test the git repo branches`, (done) => {
+      chai.request(restURL())
+        .get('repos/Anildolu/Case-Study/branches')
+        .set('Authorization', config.basicAuth)
+        .end((err, res) => {
+          res.should.have.status(200);
+          expect(res.body[0].name).to.be.equal("Anildolu-patch-1");
+          expect(res.body[0].commit.sha).to.be.equal("4bfe11708086c1db1c3806fd070a47f69a236d16");
+          expect(res.body[0].commit.url).to.be.equal("https://api.github.com/repos/Anildolu/Case-Study/commits/4bfe11708086c1db1c3806fd070a47f69a236d16");
+          expect(res.body[1].name).to.be.equal("Anildolu-patch-2");
+          expect(res.body[2].name).to.be.equal("Anildolu-patch-3");
+          expect(res.body[3].name).to.be.equal("Anildolu-patch-4");
+          expect(res.body[4].name).to.be.equal("master");
+          done();
+        });
+    })
+   
 });
--- API_Testing/spec/gitPOST_spec.js
+++ API_Testing/spec/gitPOST_spec.js
@@ -1,7 +1,7 @@
 // Require the dev-dependencies
 const chai = require('chai');
 const chaiHttp = require('chai-http');
- const config = require('../config');
+const config = require('../config');
 // const helperInstance = require('../auth');
 const expect = chai.expect;
 
@@ -13,39 +13,81 @@ let restURL = function () {
   return config.restBaseUrl + '/';
 };
 
-  describe('POST -Test Scenario', () => {
-    let postpayload={ 
-        base: "master",
-        head: "Anildolu-patch-4",
-        commit_message: "Shipped cool_feature!"
+describe('POST -Test Scenario', () => {
+  let postpayload = {
+    base: "master",
+    head: "Anildolu-patch-4",
+    commit_message: "Shipped cool_feature!"
   };
-   
-    it(`TC-01 : test the git mearge`, (done) => {
-      chai.request(restURL())
-        .post('repos/Anildolu/Case-Study/merges')
-        .send(postpayload)
-        .set('Authorization', config.basicAuth)
-        .end((err, res) => {
-          res.should.have.status(201);
-          done();
-        });
-    });
 
-    it(`TC-02 : test the git merge with invalid branch`, (done) => {
-      let postpayload1={ 
-        base: "master",
-        head: "Anildolu-patch-5",
-        commit_message: "Shipped cool_feature!"
-          };
-      chai.request(restURL())
-        .post('repos/Anildolu/Case-Study/merges')
-        .send(postpayload1)
-        .set('Authorization', config.basicAuth)
-        .end((err, res) => {
-          res.should.have.status(404);
-          expect(res.body.message).to.contains("Head does not exist");
-          expect(res.body.documentation_url).to.contains("https://developer.github.com/v3/repos/merging/#perform-a-merge");
-          done();
-        });
-    });
+  it(`TC-01 : test the git mearge`, (done) => {
+    chai.request(restURL())
+      .post('repos/Anildolu/Case-Study/merges')
+      .send(postpayload)
+      .set('Authorization', config.basicAuth)
+      .end((err, res) => {
+        res.should.have.status(201);
+        done();
+      });
+  });
+
+  it(`TC-02 : test the git merge with invalid branch`, (done) => {
+    let postpayload1 = {
+      base: "master",
+      head: "Anildolu-patch-5",
+      commit_message: "Shipped cool_feature!"
+    };
+    chai.request(restURL())
+      .post('repos/Anildolu/Case-Study/merges')
+      .send(postpayload1)
+      .set('Authorization', config.basicAuth)
+      .end((err, res) => {
+        res.should.have.status(404);
+        expect(res.body.message).to.contains("Head does not exist");
+        expect(res.body.documentation_url).to.contains("https://developer.github.com/v3/repos/merging/#perform-a-merge");
+        done();
+      });
+  });
+
+  it(`TC-03 : test the gitrepo creation`, (done) => {
+    let postpayload3 = {
+      name: "Hello-World",
+      description: "This is your first repository",
+      homepage: "https://github.com",
+      private: false,
+      has_issues: true,
+      has_projects: true,
+      has_wiki: true
+    };
+    chai.request(restURL())
+      .post('user/repos')
+      .send(postpayload3)
+      .set('Authorization', config.basicAuth)
+      .end((err, res) => {
+        res.should.have.status(201);
+        expect(res.body.name).to.be.equal("Hello-World");
+        expect(res.body.full_name).to.be.equal("Anildolu/Hello-World");
+        expect(res.body.owner.login).to.be.equal("Anildolu");
+        done();
+      });
+  });
+
+  it(`TC-04 : test the gitrepo creation with duplicate data`, (done) => {
+    let postpayload2 = {
+      name: "Hello-World",
+      description: "This is your first repository",
+      homepage: "https://github.com",
+    };
+    chai.request(restURL())
+      .post('user/repos')
+      .send(postpayload2)
+      .set('Authorization', config.basicAuth)
+      .end((err, res) => {
+        res.should.have.status(422);
+        expect(res.body.message).to.be.equal("Repository creation failed.");
+        expect(res.body.errors[0].message).to.be.equal("name already exists on this account");
+        done();
+      });
+  });
+
 });
